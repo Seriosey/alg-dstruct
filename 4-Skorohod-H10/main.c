39605082753720d78bd25c44351bad2fce99e894
Seriosey <56956007+Seriosey@users.noreply.github.com>
2022-02-27 17:24:31 +0300

    Add files via upload

---
+++ 4-Skorohod-H10/main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "AVLTree.h"
+#include <malloc.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFFER_SIZE 16
+
+int main() {
+	char command;
+	int value;
+	char buffer[BUFFER_SIZE] = { 0 };
+	char letter;
+	node* ROOT = NULL;
+	while (fgets(buffer, BUFFER_SIZE, stdin)) {
+		scanf(buffer, "%c %i", &letter, &value);
+		switch (letter) {
+		case 'a':
+			ROOT = insertel(ROOT, value);
+			break;
+		case 'r':
+			ROOT = deletel(ROOT, value);
+			break;
+		case 'f':
+			if (findel(ROOT, value))
+				puts("yes");
+			else
+				puts("no");
+			break;
+
+		}
+	}
+
+	return 0;
+}
