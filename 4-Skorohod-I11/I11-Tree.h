c1076c655f3649e1da8f25a9cb4ae6c22eede23f
Seriosey <56956007+Seriosey@users.noreply.github.com>
2022-05-12 17:16:53 +0300

    Update I11-Tree.h

--- 4-Skorohod-I11/I11-Tree.h
+++ 4-Skorohod-I11/I11-Tree.h
@@ -1,60 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
-#define T 3
-#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+
+#define T 2
 
 typedef struct node {
     struct node* parent;
-    struct node* children[2 * T + 1];
+    struct node* children[2 * T];
+    struct node* brother;
     int length;
-    int keys[2*T];
+    int keys[2 * T-1];
 } node;
-
-
-typedef struct node_list {
-    struct node_list* next;
-    struct node_list* prev;
-    node* val;
-}node_list;
-
-typedef struct split_retval {
-    node* left;
-    node* right;
-} split_retval;
-
-node_list* list_add(node_list* l, node* v);
 
 node* Node();
 
+node* insert(node* v, int x);
 
+void insert_to_node(node* v, int x);
 
 node* search(node* v, int  data);
 
-int cmpfu(const void* a, const void* b);
+node* search_min(node* v);
 
-void sort_son(node* v);
+void sort(node* v);
 
-int subtree_max(node* v);
-
-void update_keys(node* v);
-
-void split_parent(node* d);
-
-node* add(node* d, int x);
-
-node* find_brother(node* v);
-
-void delete_in_node(node* v);
-
-node* del(node* v, int x);
+node* delete(node* v, int x);
 
 int get_height(node* v);
 
-node* merge(node* l, node* r);
+node* split(node* v);
 
-node* v;
+node* merge(node* leaf);
 
-split_retval split(node* d, int key);
+node* fix(node* v);
+
+node* redistribute(node* leaf);
 
